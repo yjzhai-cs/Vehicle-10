fe597ee7a6f4d4cb43b7e5b7a2546a69130fd414
yjzhai <yjzhai.cs@gmail.com>
2024-03-28 20:08:43 +0800

    create resnet9

--- src/models/model.py
+++ src/models/model.py
@@ -1,9 +1,12 @@
 import torchvision
 from torch import nn
 from .lenet5 import LeNet5
+from .resnet9 import ResNet9
 
 def get_model(name):
-    if name == 'resnet18':
+    if name == 'resnet9':
+        net = ResNet9(in_channels=3, num_classes=10)
+    elif name == 'resnet18':
         net = torchvision.models.resnet18(num_classes=10)
     elif name == 'resnet34':
         net = torchvision.models.resnet34(num_classes=10)
