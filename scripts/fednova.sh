1a6c9de6bd52b20fc5c4acf3b4368162471f56be
yjzhai <yjzhai.cs@gmail.com>
2024-03-30 13:26:04 +0800

    update scripts

---
+++ scripts/fednova.sh
@@ -0,0 +1,23 @@
+for trial in 1
+do
+
+    python ../src/main_fednova.py --trial=$trial \
+    --rounds=100 \
+    --local_ep=10 \
+    --num_users=100 \
+    --frac=0.2 \
+    --local_bs=32 \
+    --bs=128 \
+    --lr=0.001 \
+    --momentum=0.9 \
+    --model=resnet9 \
+    --size=32 \
+    --gpu=5 \
+    --seed=42 \
+    --partition='percentage20' \
+    --datadir='../data/' \
+    --dataset=vehicle10 \
+    --print_freq=10 \
+    --local_view \
+
+done 
